98270f6d77bd9b0428109eb67aab5001125c3014
Bogdan Brandys <brandpol@poczta.onet.pl>
2024-05-17 21:03:20 +0200

    7.2 Add immutable class ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -28,5 +29,8 @@ public class StreamMain {
         System.out.println(beautifier.beautify("I even looked inside my car.", PoemBeautifier::addStars));
         System.out.println(beautifier.beautify("I've lost my glasses, I'm in need,", PoemBeautifier::replaceSpaces));
         System.out.println(beautifier.beautify("To have them now so I can read.", PoemBeautifier::changeFont));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
