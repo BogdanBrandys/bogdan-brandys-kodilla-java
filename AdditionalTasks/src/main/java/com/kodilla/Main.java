0689fa700277c6e21fd5e9c5b1e9df83e60d5b0b
Bogdan Brandys <brandpol@poczta.onet.pl>
2024-10-20 20:21:17 +0200

    28.6 matrix switch task

--- AdditionalTasks/src/main/java/com/kodilla/Main.java
+++ AdditionalTasks/src/main/java/com/kodilla/Main.java
@@ -2,8 +2,19 @@ package com.kodilla;
 
 public class Main {
     public static void main(String[] args) {
-        DecimalToHexadecimalConverter decimalToHexa = new DecimalToHexadecimalConverter();
-        String result = decimalToHexa.getHexadecimal(20);
-        System.out.println(result);
+        int[][] array = {
+                {5, 3, -2},
+                {-7, 4, 8},
+                {-2, 7, 1},
+                {9, 5, 3}
+        };
+        MatrixSwitcher switcher = new MatrixSwitcher();
+        int[][] result = switcher.switchColsToRows(array);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println();
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + "|");
+            }
+        }
     }
 }
